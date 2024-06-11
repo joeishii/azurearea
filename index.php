bd800f5353366080892a8b0a62391e8e391f71af
joeishii <150877168+joeishii@users.noreply.github.com>
2024-06-11 09:52:54 +0900

    a

--- index.php
+++ index.php
@@ -87,9 +87,10 @@ $(document).ready(function() {
                     </div>
                     <div class="row">
                         <div class="d-grid gap-2 col-6 mx-auto">
-                            <button type="button" id="btn_search" class="btn btn-primary"><h2>掲示板を検索</h2></button>
+                            <button type="button" id="btn_search" class="btn btn-danger"><h2>掲示板を検索</h2></button>
                         </div>
                     </div>
+                    <hr>
 
                     <div class="row">
                     <div class="col-sm-2"></div>
